5c0e1d63f5e152658fe87473dc4521c6c685b8c5
Danielle Atwood <danielle.atwood@ibm.com>
2021-05-27 13:53:34 -0400

    synced with other repo and ran caniuselite update

--- src/gatsby-theme-carbon/components/ArticleCardGroup/ArticleCardGroup.js
+++ src/gatsby-theme-carbon/components/ArticleCardGroup/ArticleCardGroup.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { Row, Column, ArticleCard, ImageCard } from "gatsby-theme-carbon";
 import './ArticleCardGroup.scss';
+const images = require.context('../../../images', true);
 
 export default function ArticleCardGroup({ limit, featured }) {
   const data = useStaticQuery(graphql`
@@ -23,93 +24,62 @@ export default function ArticleCardGroup({ limit, featured }) {
       }
     }
   `);
+return (
+  <>
+  <Row>
+    {data.allMdx.nodes.slice(0, 1).map((node, index) => {
+      const article = node.frontmatter;
+      //let featuredImgsrc = images(`./getting-better-network-performance-for-your-zcx-containers-featured.jpg`);
+      let imgtitle = article.title.replace(/\s+/g, '-').replace(/\//g, '').toLowerCase();
+      let featuredImgsrc = images(`./`+ imgtitle +`-featured.jpg`);
 
-  if (featured) {
+      return (
+        <Column colMd={12} colLg={12} noGutterMdLeft>
+          <ArticleCard
+            title={article.title}
+            author={article.author}
+            date={article.date}
+            readTime={article.readTime}
+            href={node.slug}
+            key={index}
+            subTitle={article.description}
+            className="featuredArticleCard"
+          >
+           <ImageCard href={node.slug}>
+           <img key={index} src={featuredImgsrc} alt={article.title}
+            /></ImageCard>
+          </ArticleCard>
+        </Column>
+      );
+      })}
+  </Row>
+  <Row>
+  {data.allMdx.nodes.slice(1, 10).map((node, index) => {
+    const article = node.frontmatter;
+    //let imgsrc = images(`./gallery.jpg`);
+    const defaultImg = images(`./gallery.jpg`);
+       let imgtitle = article.title.replace(/\s+/g, '-').replace(/\/|\?|'|\(|\)/g, '').toLowerCase();
+       let imgsrc = images(`./`+ imgtitle +`.jpg`);
     return (
-      <>
-      <Row>
-        {data.allMdx.nodes.slice(0, 1).map((node, index) => {
-          const article = node.frontmatter;
-          const images = require.context('../../../images', true);
-          let imgtitle = article.title.replace(/\s+/g, '-').replace(/\//g, '').toLowerCase();
-
-          let featuredImgsrc = images(`./`+ imgtitle +`-featured.jpg`);
-          //let featuredImgsrc = images(`./square.jpg`);
-
-          return (
-            <Column colMd={12} colLg={12} noGutterMdLeft>
-              <ArticleCard
-                title={article.title}
-                author={article.author}
-                date={article.date}
-                readTime={article.readTime}
-                href={node.slug}
-                key={index}
-                subTitle={article.description}
-                className="featuredArticleCard"
-              >
-               <ImageCard href={node.slug}> 
-               <img key={index} src={featuredImgsrc} alt={article.title}
-                /></ImageCard> 
-              </ArticleCard>
-            </Column>
-          );
-        })}
-      </Row>
-      <Row>
-      {data.allMdx.nodes.slice(1, 10).map((node, index) => {
-        const article = node.frontmatter;
-        const images = require.context('../../../images', true);
-        const defaultImg = images(`./gallery.jpg`);
-        let imgtitle = article.title.replace(/\s+/g, '-').replace(/\/|\?|'|\(|\)/g, '').toLowerCase();
-        let imgsrc = images(`./`+ imgtitle +`.jpg`);
-        
-
-        return (
-          <Column colMd={4} colLg={6} colXl={4} noGutterMdLeft>
-            <ArticleCard
-              title={article.title}
-              author={article.author}
-              date={article.date}
-              readTime={article.readTime}
-              href={node.slug}
-              key={index}
-            >
-              <img
-                src={imgsrc}
-                alt={article.title}
-                onError={(e)=>{e.target.onerror = null; e.target.src=defaultImg}}
-              />
-            </ArticleCard>
-          </Column>
-        );
-      })}
-    </Row>
-    </>
+      <Column colMd={4} colLg={4} noGutterMdLeft>
+        <ArticleCard
+          title={article.title}
+          author={article.author}
+          date={article.date}
+          readTime={article.readTime}
+          href={node.slug}
+          key={index}
+        >
+          <img
+            src={imgsrc}
+            alt={article.title}
+            onError={(e)=>{e.target.onerror = null; e.target.src=defaultImg}}
+          />
+        </ArticleCard>
+      </Column>
     );
-  }
-  return (
-    <Row>
-      {data.allMdx.nodes.slice(0, 3).map((node, index) => {
-        const article = node.frontmatter;
-        return (
-          <Column colMd={4} colLg={6} colXl={4} noGutterMdLeft>
-            <ArticleCard
-              title={article.title}
-              author={article.author}
-              date={article.date}
-              readTime={article.readTime}
-              href={node.slug}
-              key={index}
-            >
-              <img
-                src="https://gatsby-theme-carbon.now.sh/static/ebd956040be4c438af81a5204152b45c/0c3f5/Article_06.png"
-                alt={article.title}
-              />
-            </ArticleCard>
-          </Column>
-        );
-      })}
-    </Row>
-  );
+  })}
+</Row>
+</>
+);
 }
